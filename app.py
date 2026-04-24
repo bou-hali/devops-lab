8a6dfb26b399a70a2b4196be5b4d753ba8b8b7db
hajar <hajarbouhali07@gmail.com>
2026-04-24 10:08:47 +0100

    update

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Version lala hajar cherifa bouhalia ra2i3a - Deplo .."
+    return "Version lala hajar cherifa bouhalia ra2i3a aishato - Deplo .."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
